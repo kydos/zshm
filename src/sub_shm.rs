706d468ea646c42e386117f5851900ea0b784c42
kydos <kydos@protonmail.com>
2025-07-01 14:16:37 +0200

    added basic examples on shm pub/sub

---
+++ src/sub_shm.rs
@@ -0,0 +1,20 @@
+use zenoh::Wait;
+fn main() {
+    let z = zenoh::open(zenoh::Config::default())
+        .wait()
+        .expect("Failed to open Zenoh session");    
+
+    let sub = z.declare_subscriber("zenoh/shm/buffer")
+        .wait()
+        .expect("Failed to declare subscriber");
+
+    while let Ok(s) = sub.recv() {
+        let buf = s.payload();
+        
+        let is_shm = if buf.as_shm().is_none() { false } else { true };            
+
+        buf.try_to_string()
+            .map(|s| println!("Received (SHM: {}): {}",is_shm, s))
+            .unwrap_or_else(|_| println!("Received non-string payload"));                       
+    }
+}
