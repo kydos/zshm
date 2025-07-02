3020cf9d8c3cd544642e76d08677db07570a7fc9
yellowhatter <bannov.dy@gmail.com>
2025-07-02 09:43:38 +0300

    polish producer consumer examples

--- src/polling_producer.rs
+++ src/polling_producer.rs
@@ -1,8 +1,9 @@
-use std::sync::atomic::{AtomicUsize};
+use std::sync::atomic::AtomicUsize;
 
 use rand::random;
 use zenoh::{
-    shm::{ AllocAlignment, ShmBuf, ShmProviderBuilder, ZShm}, Wait
+    Wait,
+    shm::{AllocAlignment, ShmProviderBuilder, ZShm},
 };
 
 // Shared data
@@ -13,10 +14,7 @@ pub struct SharedData {
 }
 
 fn main() {
-    let z = zenoh::open(zenoh::Config::default())
-        .wait()
-        .expect("Failed to open Zenoh session");
-
+    // get alignment for SharedData type by means of new API
     let alignment = AllocAlignment::for_type::<SharedData>();
     let size = std::mem::size_of::<SharedData>();
 
@@ -24,66 +22,68 @@ fn main() {
         .with_alignment(alignment)
         .wait()
         .unwrap();
-    
-    let buf = shm_provider
+
+    let mut buf = shm_provider
         .alloc(size)
         .with_alignment(alignment)
         .wait()
         .unwrap();
 
-    // initialize data        
-    let shared_data = unsafe { 
-         let ptr = buf.as_ptr() as *const SharedData;
-        &*ptr 
+    // initialize data
+    let shared_data = unsafe {
+        let ptr = buf.as_mut_ptr() as *mut SharedData;
+        let shared_data = &mut *ptr;
+
+        shared_data
+            .len
+            .store(0, std::sync::atomic::Ordering::Release);
+        shared_data
     };
-    
-    shared_data.len
-        .store(0, std::sync::atomic::Ordering::Release);
 
     // change the morph of buf to be able to make it's copies
     let buf: ZShm = buf.into();
 
-    // shallow copy to move in producer thread
-    let mut buf_in_thread = buf.clone();
+    // shallow copy to move in responder thread
+    let buf_in_thread = buf.clone();
     let tid = std::thread::spawn(move || {
-        
-        let shared_data = unsafe { 
-            let ptr = buf_in_thread.as_mut_unchecked().as_mut_ptr() as *mut SharedData;
-            &mut *ptr 
-        };
+        let z = zenoh::open(zenoh::Config::default())
+            .wait()
+            .expect("Failed to open Zenoh session");
 
-        loop {
-            let len = shared_data.len.load(std::sync::atomic::Ordering::Acquire);
-            if len == 0 {    
-                let mut sum: usize = 0;
-                let len = (512 + random::<u32>() % 513) as usize; // 
-                for i in 0..len {
-                    let r: u8 = rand::random();
-                    shared_data.data[i] = r;
-                    sum += r as usize;
-                }
+        let queryable = z
+            .declare_queryable("shm/polling/buffer")
+            .wait()
+            .expect("Failed to declare queryable");
 
-                println!("Produced buffer of {len} bytes with sum of {sum}");
-                shared_data.len
-                    .store(len, std::sync::atomic::Ordering::Release);
-            } else {
-                // Wait until the data is consumed
-                std::thread::sleep(std::time::Duration::from_millis(100));
-            }
+        while let Ok(query) = queryable.recv() {
+            query
+                .reply("shm/polling/buffer", buf_in_thread.clone())
+                .wait()
+                .expect("Failed to reply to query");
         }
     });
 
-    let queryable = z
-        .declare_queryable("shm/polling/buffer")
-        .wait()
-        .expect("Failed to declare queryable");
+    // producer loop
+    while !tid.is_finished() {
+        let len = shared_data.len.load(std::sync::atomic::Ordering::Acquire);
+        if len == 0 {
+            let mut sum: usize = 0;
+            let len = (512 + random::<u32>() % 513) as usize; // 
+            for i in 0..len {
+                let r: u8 = rand::random();
+                shared_data.data[i] = r;
+                sum += r as usize;
+            }
 
-    while let Ok(query) = queryable.recv() {
-        query
-            .reply("shm/polling/buffer", buf.clone())
-            .wait()
-            .expect("Failed to reply to query");
+            println!("Produced buffer of {len} bytes with sum of {sum}");
+            shared_data
+                .len
+                .store(len, std::sync::atomic::Ordering::Release);
+        } else {
+            // Wait until the data is consumed
+            std::thread::sleep(std::time::Duration::from_millis(100));
+        }
     }
 
-    tid.join().expect("Producer thread panicked");
+    tid.join().expect("Responder thread panicked");
 }
